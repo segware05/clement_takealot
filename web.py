0f7d51853ca5f037ba4461b66aaa7cada4e03b86
segware05 <223298131@edu.vut.ac.za>
2026-04-14 20:40:17 +0200

    Add files via upload

---
+++ web.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+import pandas as pd
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("base.html")
+
+@app.route('/howto')
+def howto():
+    return render_template("howto.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
